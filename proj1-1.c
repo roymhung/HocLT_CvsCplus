4f1b40e6dce4634c49924d06054ab40b0dbf17b1
roymhung <lemanhhung1555@gmail.com>
2025-05-25 21:58:57 +0700

    phiên bản c thuần

---
+++ proj1-1.c
@@ -0,0 +1,17 @@
+// day la bai code dau tien cua mhung coder
+#include <stdio.h>
+
+int main() {
+    
+    // %d/d: decimal-so thap phan
+    // %c/c: char-ki tu
+    // đổi %d thanh %c nếu muốn in biến char
+    // string: chuỗi kí tự nhưng chỉ có ở C++ còn C sễ sử dụng: char ten_bien[sokitu]
+	int tuoi = 0;
+    char xep_loai = 'A';
+    char ho_ten[50] = "roy hung123";
+	printf("hung nam nay %d tuoi\n", tuoi);
+	printf("hung duoc xep loai %c\n", xep_loai);
+	printf("hung duoc xep loai %s\n", ho_ten);
+
+}
