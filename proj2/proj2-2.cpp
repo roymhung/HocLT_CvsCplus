cd531c80cc8ccdee4a0670ae2dcc283e5ff506f1
roymhung <lemanhhung1555@gmail.com>
2025-05-28 20:06:52 +0700

    logic operator

--- proj2/proj2-2.cpp
+++ proj2/proj2-2.cpp
@@ -4,47 +4,10 @@
 using namespace std;
 
 int main() {
-    int age;
-    string fullname;
-    string country;
-    char xep_loai_tot_nghiep;
-    float height;
-    float weight;
+    bool a = true;
+    bool b = false;
+    bool c = false;
 
-    // Nhập họ tên
-    cout << "Nhap ten: ";
-    getline(cin, fullname);
-
-  // Nhập năm sinh
-    cout << "Nhap nam sinh: ";
-    cin >> age;
-
-    cin.ignore();
-
-    // Nhập nơi ở
-    cout << "Nhap noi o: ";
-    getline(cin, country);
-
-    // Nhập chiều cao
-    cout << "Nhap chieu cao: ";
-    cin >> height;
-
-    // Nhập cân nặng
-    cout << "Nhap can nang: ";
-    cin >> weight;
-
-    // Nhập xếp loại tốt nghiệp
-    cout << "Nhap xep loai tot nghiep: ";
-    cin >> xep_loai_tot_nghiep;
-
-    // Hiển thị thông tin
-    cout << "\n--- Thong tin ca nhan ---\n";
-    cout << "Xin chao. Toi la: " << fullname ;
-    cout << "\nNam sinh: " << age << " tuoi" ;
-    cout << "\nToi den tu: " << country ;
-    cout << "\nToi cao: " << height << " cm";
-    cout << "\nToi nang: " << weight << " kg" ;
-    cout << "\nToi xep loai tot nghiep: " << xep_loai_tot_nghiep;
-
-    return 0;
+    bool d = a || b && c;
+    cout << d;
 }
