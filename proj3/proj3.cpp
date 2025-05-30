292bf2919d48112442a1ff18a6bb95947bb4fdb9
roymhung <lemanhhung1555@gmail.com>
2025-05-30 21:16:05 +0700

    bai2

--- proj3/proj3.cpp
+++ proj3/proj3.cpp
@@ -5,26 +5,20 @@ using namespace std;
 int main(){
     float x = 2;
     float y = 3;
-
-    float z;
-    
-    // z = x*y + (float)x/y;
-
-    // z = x + (1/(x+(1/(x+(1/(x + y))))));
-
-    // z = sqrt((pow((3*x + 2*y),2)) * (pow((5*x + 1),3)));
-
-    float a;
-    float b;
     float c;
+    float s;
+    float r;
 
-     a = pow((3*x + 2*y),2);
+    const float z = 3.14;
 
-    b = pow((5*x + 1),3);
+    cout << "Nhap ban kinh: ";
+    cin >> r;
 
-    c = a* b;
-    z = sqrt(c);
-    
-    cout << z;
-    cout << z;
+    c = 2 * z * r;
+
+    s = z * pow(r,2);
+
+    cout << "Chu vi hinh tron: " << c << "\n";
+    cout << "Dien tich hinh tron: " << s;
+   
 }
