f54d164b1ae820a941e24913b83c832192808319
royhung <lemanhhung1555@gmail.com>
2025-06-07 21:57:31 +0700

    bai 4.6

--- proj4/proj4.cpp
+++ proj4/proj4.cpp
@@ -1,26 +1,26 @@
 #include <iostream>
+#include<cmath>
+
 using namespace std;
 
 int main() {
-    int km;
-    long tien = 0;
+    int n;
 
-    cout << "Nhap so km: ";
-    cin >> km;
+    cout << "Nhap so n: ";
+    cin >> n;
 
 
-    if (km <= 0) {
-        cout << "So tien khong hop le! \n";
-    } else if(km == 1){
-        tien = 15000;
-    }else if(km <= 30){
-        tien = 15000 + (km - 1) * 13000;
+    if (n <= 0) {
+        cout << "Vui long nhap so nguyen duong! \n";
+        return 0;
+    } 
+
+    int can = sqrt(n);
+
+    if (can * can == n) {
+        cout << "Ket qua: " << n << " la so chinh phuong\n";
     }else{
-        tien = 15000 + (29 * 13000) + (km - 30) * 10000;
-    }
-
-    if (km > 0) {
-        cout << "So tien phai tra: " << tien << " VND\n";
+        cout << "Ket qua: " << n << " khong la so chinh phuong\n";
     }
     return 0;
 }
