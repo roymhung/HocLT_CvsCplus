ef993303ce7570b2bd352d409505b72ab9f5e8c5
royhung <lemanhhung1555@gmail.com>
2025-06-07 21:49:12 +0700

    bai 4.5

--- proj4/proj4.cpp
+++ proj4/proj4.cpp
@@ -2,33 +2,25 @@
 using namespace std;
 
 int main() {
-    int gio, phut, giay;
+    int km;
+    long tien = 0;
 
-    cout << "Nhap gio: ";
-    cin >> gio;
+    cout << "Nhap so km: ";
+    cin >> km;
 
-    cout << "Nhap phut: ";
-    cin >> phut;
 
-    cout << "Nhap giay: ";
-    cin >> giay;
-
-    if (gio >= 0 && gio <= 23 && phut >= 0 && phut <= 59 && giay >= 0 && giay <= 59) {
-        cout << "Ket qua: Hop le\n";
-    } else {
-        cout << "Ket qua: Khong hop le\n";
-
-        // Giải thích lý do không hợp lệ
-        if (gio < 0 || gio > 23) {
-            cout << "Do khong co gio " << gio << " nha ae :))\n";
-        }
-        if (phut < 0 || phut > 59) {
-            cout << "Do khong co phut " << phut << " nha ae :))\n";
-        }
-        if (giay < 0 || giay > 59) {
-            cout << "Do khong co giay " << giay << " nha ae :))\n";
-        }
+    if (km <= 0) {
+        cout << "So tien khong hop le! \n";
+    } else if(km == 1){
+        tien = 15000;
+    }else if(km <= 30){
+        tien = 15000 + (km - 1) * 13000;
+    }else{
+        tien = 15000 + (29 * 13000) + (km - 30) * 10000;
     }
 
+    if (km > 0) {
+        cout << "So tien phai tra: " << tien << " VND\n";
+    }
     return 0;
 }
