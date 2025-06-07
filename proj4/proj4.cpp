6a790d719f7563e32557134494129b9462d6610e
royhung <lemanhhung1555@gmail.com>
2025-06-07 20:52:36 +0700

    bai 4.2

--- proj4/proj4.cpp
+++ proj4/proj4.cpp
@@ -1,32 +1,58 @@
-#include<iostream>
-
+#include <iostream>
 using namespace std;
 
-int main(){
-    float x, y;
+int main() {
+    float x, y, z;
 
     cout << "Nhap x: ";
     cin >> x;
     cout << "Nhap y: ";
     cin >> y;
+    cout << "Nhap z: ";
+    cin >> z;
 
-    if(x == 0){
-        cout << "x bang 0" << "\n";
-    }else{
-        cout << "x khac 0" << "\n";
+    // Tìm số lớn nhất
+    float maxVal = x;
+    if (y > maxVal) maxVal = y;
+    if (z > maxVal) maxVal = z;
+    cout << "Ket qua 1: So Lon Nhat la " << maxVal << "\n";
+
+    // Tìm số bé nhất
+    float minVal = x;
+    if (y < minVal) minVal = y;
+    if (z < minVal) minVal = z;
+    cout << "Ket qua 2: So Be Nhat la " << minVal << "\n";
+
+    // Kiểm tra cùng dấu
+    if ((x > 0 && y > 0 && z > 0) || (x < 0 && y < 0 && z < 0)) {
+        cout << "Ket qua 3: Ca 3 so cung dau\n";
+    } else {
+        cout << "Ket qua 3: Ca 3 so khac dau\n";
     }
 
-    if(y == 0){
-        cout << "y bang 0" << "\n";
-    }else{
-        cout << "y khac 0" << "\n";
+    // In ra các cặp trái dấu
+    cout << "Ket qua 4: ";
+    bool coCapTraiDau = false;
+
+    if (x * y < 0) {
+        cout << "(" << x << "," << y << ")";
+        coCapTraiDau = true;
+    }
+    if (x * z < 0) {
+        if (coCapTraiDau) cout << ", ";
+        cout << "(" << x << "," << z << ")";
+        coCapTraiDau = true;
+    }
+    if (y * z < 0) {
+        if (coCapTraiDau) cout << ", ";
+        cout << "(" << y << "," << z << ")";
+        coCapTraiDau = true;
+    }
+    if (!coCapTraiDau) {
+        cout << "Khong co cap so nao trai dau";
     }
 
-    if(x != 0 && y != 0){
-        if((x > 0 && y > 0) || (x < 0 && y < 0)){
-            cout << "hai so cung dau" << "\n";
-        }else{
-            cout << "hai so khac dau" << "\n";
-        }
-    }
+    cout << "\n";
+
+    return 0;
 }
