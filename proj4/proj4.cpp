6a52ccaca533eb2d6a405f5bd492b73df307ee1a
royhung <lemanhhung1555@gmail.com>
2025-06-07 20:20:07 +0700

    bai 4.1

--- proj4/proj4.cpp
+++ proj4/proj4.cpp
@@ -3,24 +3,30 @@
 using namespace std;
 
 int main(){
-    int tuoi = 0;
-    cout << "Nhap so tuoi: ";
-    cin >> tuoi;
-    
-    if(tuoi >=18){
-        cout << "Du tuoi di tu.";
-    }
-    else if(tuoi >= 15 && tuoi <= 17)
-    {
-        cout << "Di trai cai tao";
-    }
-    else if(tuoi >= 11 && tuoi <= 14)
-    {
-        cout << "Ca quan tai gia";
-    }
-    else
-    {
-        cout << "Tu do";
+    float x, y;
 
+    cout << "Nhap x: ";
+    cin >> x;
+    cout << "Nhap y: ";
+    cin >> y;
+
+    if(x == 0){
+        cout << "x bang 0" << "\n";
+    }else{
+        cout << "x khac 0" << "\n";
+    }
+
+    if(y == 0){
+        cout << "y bang 0" << "\n";
+    }else{
+        cout << "y khac 0" << "\n";
+    }
+
+    if(x != 0 && y != 0){
+        if((x > 0 && y > 0) || (x < 0 && y < 0)){
+            cout << "hai so cung dau" << "\n";
+        }else{
+            cout << "hai so khac dau" << "\n";
+        }
     }
 }
