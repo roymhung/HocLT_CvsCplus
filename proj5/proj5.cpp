2899f49e9a6563376fd3ee84e2bf96b59eaada97
royhung <lemanhhung1555@gmail.com>
2025-06-11 18:59:54 +0700

    demo while loop

--- proj5/proj5.cpp
+++ proj5/proj5.cpp
@@ -2,9 +2,14 @@
 using namespace std;
 
 int main() {
-    for(int i = 10; i > 0; i--){
-        cout << "anh yeu em" << i << "\n";
-    }    
+    // for(int i = 10; i > 0; i--){
+    //     cout << "anh yeu em " << i << "\n";
+    // }    
 
+    int i = 0;
+    while(i <= 10){
+        cout << "anh yeu em " << i << "\n";
+        i++;
+    }
     return 0;
 }
