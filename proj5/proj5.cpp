029c6ea803690914c6c0d097e845c097c7edb39c
royhung <lemanhhung1555@gmail.com>
2025-06-12 20:24:49 +0700

    dk chua chat

--- proj5/proj5.cpp
+++ proj5/proj5.cpp
@@ -1,37 +1,39 @@
 #include <iostream>
 using namespace std;
 
+// Hàm tính UCLN bằng thuật toán Euclid
+int ucln(int a, int b) {
+    while (b != 0) {
+        int r = a % b;
+        a = b;
+        b = r;
+    }
+    return a;
+}
+
+// Hàm tính BCNN từ UCLN
+int bcnn(int a, int b) {
+    return (a * b) / ucln(a, b);
+}
+
 int main() {
-    int n;
-    int count = 0;
-    
-    // Nhập số nguyên dương n
-    do {
-        cout << "Nhap n: ";
-        cin >> n;
-    } while (n <= 0);
-    
-    // Câu 1: In ra các số mà n chia hết từ 1 đến n
-    cout << "Ket qua 1: ";
-    for (int i = 1; i <= n; i++) {
-        if (n % i == 0) {
-            cout << i << " ";
-            count++; // Đếm số lượng ước
-        }
+    int a, b;
+    cout << "Nhap a: ";
+    cin >> a;
+    cout << "Nhap b: ";
+    cin >> b;
+
+    // Kiểm tra số hợp lệ
+    if (a <= 0 || b <= 0) {
+        cout << "Vui long nhap hai so nguyen duong!" << endl;
+        return 0;
     }
-    cout << endl;
-    
-    // Câu 2: In ra số lượng ước
-    cout << "Ket qua 2: " << count << endl;
-    
-    // Câu 3: Kiểm tra số nguyên tố
-    if (n == 1) {
-        cout << "Ket qua 3: Ko phai so nguyen to" << endl;
-    } else if (count == 2) {
-        cout << "Ket qua 3: La so nguyen to" << endl;
-    } else {
-        cout << "Ket qua 3: Ko phai so nguyen to" << endl;
-    }
-    
+
+    int u = ucln(a, b);
+    int bcn = bcnn(a, b);
+
+    cout << "Uoc chung lon nhat la: " << u << endl;
+    cout << "Boi chung nho nhat la: " << bcn << endl;
+
     return 0;
 }
