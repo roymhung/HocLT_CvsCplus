823f439d701591f487451464c93fb1aaf781032d
royhung <lemanhhung1555@gmail.com>
2025-06-13 17:28:19 +0700

    bai 5.6

--- proj5/proj5.cpp
+++ proj5/proj5.cpp
@@ -1,42 +1,21 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
-// Hàm tìm UCLN bằng thuật toán Euclid
-int gcd(int a, int b) {
-    while (b != 0) {
-        int temp = b;
-        b = a % b;
-        a = temp;
-    }
-    return a;
-}
-
-// Hàm tìm BCNN
-int lcm(int a, int b) {
-    return (a * b) / gcd(a, b);
-}
-
 int main() {
-    int a, b;
-    
-    // Nhập hai số nguyên dương a và b
-    do {
-        cout << "Nhap a: ";
-        cin >> a;
-    } while (a <= 0);
-    
-    do {
-        cout << "Nhap b: ";
-        cin >> b;
-    } while (b <= 0);
-    
-    // Tính và in UCLN
-    int ucln = gcd(a, b);
-    cout << "Uoc chung lon nhat la: " << ucln << endl;
-    
-    // Tính và in BCNN
-    int bcnn = lcm(a, b);
-    cout << "Boi chung nho nhat la: " << bcnn << endl;
-    
+    int n;
+    cout << "Nhap n: ";
+    cin >> n;
+
+    int sum = 0;
+    int x = 0;
+
+    // vong lap de tim X
+    while (sum <= n) {
+        x++; // tang x len 1
+        sum += x; // Cong x vao tong
+    }
+
+    cout << "X nho nhat la: " << x;
     return 0;
 }
