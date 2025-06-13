23025a86d46e23e512884bb61d159b54069b5e6a
royhung <lemanhhung1555@gmail.com>
2025-06-13 18:01:07 +0700

    bai 5.7 - no function

--- proj5/proj5.cpp
+++ proj5/proj5.cpp
@@ -6,16 +6,22 @@ int main() {
     int n;
     cout << "Nhap n: ";
     cin >> n;
-
+    int tempSum;
     int sum = 0;
     int x = 0;
 
-    // vong lap de tim X
-    while (sum <= n) {
-        x++; // tang x len 1
-        sum += x; // Cong x vao tong
+    for (int i = 1; i <= n; i++) {
+        if (i >= 5) {
+            tempSum = 2 * i * i + 5 * i + 9;
+        }
+        else {
+            tempSum = -2 * i * i + 4 * i - 9;
+        }
+        sum += tempSum;
+        cout << "tempSum nho nhat la: " << tempSum << "\n";
     }
 
-    cout << "X nho nhat la: " << x;
+    
+    cout << "sum nho nhat la: " << sum;
     return 0;
 }
