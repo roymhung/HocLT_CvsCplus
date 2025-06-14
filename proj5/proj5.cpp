900281f5b2fa3a37c036a7526e68e9ba1aa7d54f
royhung <lemanhhung1555@gmail.com>
2025-06-14 18:35:07 +0700

    bai 5.9 theo hang

--- proj5/proj5.cpp
+++ proj5/proj5.cpp
@@ -3,9 +3,9 @@
 using namespace std;
 
 int main() {
-    for (int i = 1; i <= 10; i++) {          // Dòng: từ 1 đến 10
-        for (int j = 1; j <= 10; j++) {      // Cột: từ 1 đến 10
-            cout << setw(6) << j << "x" << i << "=" << j * i;
+    for (int i = 1; i <= 10; i++) {           // Dòng: từng bảng cửu chương (1x1 → 10x10)
+        for (int j = 1; j <= 10; j++) {       // Cột: từng phép nhân
+            cout << setw(6) << i << "x" << j << "=" << i*j;
         }
         cout << endl;
     }
